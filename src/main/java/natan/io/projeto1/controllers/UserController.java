3c9fc621a69eee40e7543c3aa1205fbff4416590
mauricio-grando <mauricioa.grando@gmail.com>
2018-12-03 02:08:05 -0200

    Projeto modulo 2

---
+++ src/main/java/natan/io/projeto1/controllers/UserController.java
@@ -0,0 +1,25 @@
+package natan.io.projeto1.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import natan.io.projeto1.service.UserService;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/user")
+	public String getUsers(Model model) {
+		
+		model.addAttribute("usersList", this.userService.findAll());
+		
+		return "user";
+		
+	}
+	
+}
